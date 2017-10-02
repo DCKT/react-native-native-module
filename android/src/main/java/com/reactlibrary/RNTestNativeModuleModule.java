e80e9badd5302a39a7770c93cf4919064a492006
Thomas Deconinck <thomas.d@colisweb.com>
2017-10-02 21:49:23 +0200

    Add example

--- android/src/main/java/com/reactlibrary/RNTestNativeModuleModule.java
+++ android/src/main/java/com/reactlibrary/RNTestNativeModuleModule.java
@@ -5,6 +5,8 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.reactlibrary.BuildConfig;
+import com.facebook.react.bridge.Promise;
 
 public class RNTestNativeModuleModule extends ReactContextBaseJavaModule {
 
@@ -18,5 +20,15 @@ public class RNTestNativeModuleModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNTestNativeModule";
+  }
+
+  @ReactMethod
+  public void getAppVersionCallback(Callback callback) {
+    callback.invoke(BuildConfig.VERSION_CODE);
+  }
+
+  @ReactMethod
+  public void getAppVersionPromise(Promise promise) {
+    promise.resolve(BuildConfig.VERSION_CODE);
   }
 }
